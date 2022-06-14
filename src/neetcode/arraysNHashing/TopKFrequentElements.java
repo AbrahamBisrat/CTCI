66f3ff063e2740c383e2d650ae08612a167e6e70
Abraham Bisrat <abrahammehari0@gmail.com>
2022-06-14 03:46:53 -0500

    Added condensed version

--- src/neetcode/arraysNHashing/TopKFrequentElements.java
+++ src/neetcode/arraysNHashing/TopKFrequentElements.java
@@ -44,4 +44,18 @@ public class TopKFrequentElements {
 			resultSet.add(minHeap.poll().getKey());
 		return resultSet;
 	}
+	private static List<Integer> topKFrequentCondensed(int[] numbers, int k) {
+		if(numbers.length==0) return new ArrayList<>();
+		Map<Integer, Integer> freqMap = new HashMap<>();
+		for(int number : numbers) freqMap.put(number, freqMap.getOrDefault(number, 0) + 1);
+		PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+		for(Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+			minHeap.add(entry);
+			if(minHeap.size() > k) minHeap.remove();
+		}
+		List<Integer> resultSet = new ArrayList<>();
+		while(!minHeap.isEmpty()) resultSet.add(minHeap.poll().getKey());
+		return resultSet;
+	}
+	
 }
