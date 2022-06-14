dc964ddba9e7ad995f407164720122b5b4173bd4
Abraham Bisrat <abrahammehari0@gmail.com>
2022-06-14 03:45:02 -0500

    Added Top K frequent occurences in a list

--- src/neetcode/arraysNHashing/TopKFrequentElements.java
+++ src/neetcode/arraysNHashing/TopKFrequentElements.java
@@ -1,7 +1,6 @@
 package neetcode.arraysNHashing;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,32 +16,32 @@ import java.util.PriorityQueue;
 public class TopKFrequentElements {
 	public static void p(Object line) { System.out.println(line); }
 	public static void main(String[] args) {
-		p(topKFrequent(new int[] {1, 1, 1, 2, 2, 3, 3, 3, 3}, 2));
+		p(topKFrequent(new int[] {1, 1, 1, 2, 2, 3, 3, 3, 3}, 1));
 	}
 	/* make a frequency map
 	 * go through the sizes and append them to minHeap,
 	 * if the size of the heap is greater than k, remove */
 	private static List<Integer> topKFrequent(int[] numbers, int k) {
-		if(k >= numbers.length) return new ArrayList<>();
+		if(numbers.length==0) return new ArrayList<>();
 		Map<Integer, Integer> freqMap = new HashMap<>();
 		
 		for(int number : numbers)
 			freqMap.put(number, freqMap.getOrDefault(number, 0) + 1);
+		p(freqMap);
 		
-		/* go through all numbers of map and push them in a minHeap which will have a top k elements 
-		 * If at any time it has > k elements, then remove the smallest element. */
-		PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>(Collections.reverseOrder());
-		
+		/* Here I am going to use a bit of magic.
+		 * PriorityQueue but rather sorting it via the key */
+		PriorityQueue<Map.Entry<Integer, Integer>> minHeap = 
+				new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
 		for(Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
 			minHeap.add(entry);
 			if(minHeap.size() > k)
-				minHeap.poll();
-			p(minHeap);
+				minHeap.remove();
 		}
-		// Create a list of k elements
-		List<Integer> topKResults = new ArrayList<>();
+		p(minHeap);
+		List<Integer> resultSet = new ArrayList<>();
 		while(!minHeap.isEmpty())
-			topKResults.add(minHeap.poll().getKey());
-		return topKResults;
+			resultSet.add(minHeap.poll().getKey());
+		return resultSet;
 	}
 }
