8fd9526bafd0fc6c2b131d26b15cbba79c08d7c7
Abraham Bisrat <abrahammehari0@gmail.com>
2022-06-21 23:33:31 -0500

    clean up

--- src/neetcode/RemoveAdjacentDuplicate.java
+++ src/neetcode/RemoveAdjacentDuplicate.java
@@ -3,20 +3,21 @@ package neetcode;
 import java.util.Stack;
 
 public class RemoveAdjacentDuplicate {
-	public static void p(Object line) { System.out.println(line); }
+	public static void p(Object line) {
+		System.out.println(line);
+	}
+
 	public static void main(String[] args) {
 		p(removeAdj("10010**1010"));
 	}
+
 	private static int removeAdj(String input) {
-		if(input.length() == 0 || input == null) return 0;
+		if (input.length() == 0 || input == null) return 0;
 		Stack<Character> stack = new Stack<>();
-		for(char letter : input.toCharArray()) {
-			if(!stack.isEmpty() &&
-					(stack.peek() == '1' && letter == '0'
-					|| stack.peek() == '0' && letter == '1')) {
+		for (char letter : input.toCharArray())
+			if (!stack.isEmpty() && (stack.peek() == '1' && letter == '0' || stack.peek() == '0' && letter == '1'))
 				stack.pop();
-			} else stack.push(letter);
-		}
+			else stack.push(letter);
 		return stack.size();
 	}
 }
