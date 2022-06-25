7d3dd286e0c43eb1694a3f1866a3f5357de71683
Abraham Bisrat <abrahammehari0@gmail.com>
2022-06-24 23:11:31 -0500

    Added word search

--- src/neetcode/dynamicProgramming/WordSearch.java
+++ src/neetcode/dynamicProgramming/WordSearch.java
@@ -23,31 +23,32 @@ public class WordSearch {
 		
 	}
 	static boolean wordSearch(char[][] grid, String target) {
-		int col = grid.length;
-		int row = grid[0].length;
-		for(int i = 0; i < col; i++)
-			for(int j = 0; j < row; j++)
-				if(grid[i][j] == target.charAt(0) && searchWord(grid, i, j, 0, target))
-					return true;
-		return false;
+		for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(target.charAt(0) == grid[i][j] && wordSearch(grid, target, i, j, 0))
+                    return true;
+            }
+        }
+        return false;
 	}
-	static boolean searchWord(char[][] grid, int i, int j, int index, String target) {
-		if(index == target.length()) return true;
-		
-		// termination cases;
-		if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length 
-				|| grid[i][j] != target.charAt(index)) {
-			return false;
-		}
-		int temp = grid[i][j];
-		grid[i][j] = ' ';
-		// go to all cardinal directions, return true if found
-		boolean found = searchWord(grid, i+1, j, index+1, target) || // left
-					    searchWord(grid, i-1, j, index+1, target) || // right
-					    searchWord(grid, i, j+1, index+1, target) || // up
-					    searchWord(grid, i, j-1, index+1, target); //down
-		// set it to not visited
-		grid[i][j] = (char)temp;
-		return found;
-	}
+	static boolean wordSearch(char[][] board, String word, int i, int j, int count) {
+        if(word.length() == count) return true;
+        if(i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != word.charAt(count))
+            return false;
+        char temp = board[i][j];
+        board[i][j] = ' ';
+        
+        // search to left
+        boolean left = wordSearch(board, word, i - 1, j, count+1);
+        // search to the right
+        boolean right = wordSearch(board, word, i + 1, j, count+1);
+        // search top
+        boolean top = wordSearch(board, word, i, j + 1, count+1);
+        // search bottom
+        boolean bottom = wordSearch(board, word, i, j - 1, count+1);
+        
+        board[i][j] = temp;
+        if(left || right || top || bottom) return true;
+        return false;
+    }
 }
