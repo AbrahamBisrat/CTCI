9cbca3d464bad51067c75846fb2618360761324e
Abraham Bisrat <abrahammehari0@gmail.com>
2022-07-13 10:58:54 -0500

    update int to roman

--- src/amazon/IntegerToRoman.java
+++ src/amazon/IntegerToRoman.java
@@ -14,46 +14,51 @@ import java.util.Queue;
 	C             100
 	D             500
 	M             1000
-	For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 	
-	Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+	For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII,
+	which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 	
-	I can be placed before V (5) and X (10) to make 4 and 9. 
-	X can be placed before L (50) and C (100) to make 40 and 90. 
+	Roman numerals are usually written largest to smallest from left to right.
+	However, the numeral for four is not IIII. Instead, the number four is written as IV.
+	Because the one is before the five we subtract it making four. The same principle applies to the number nine,
+	which is written as IX. There are six instances where subtraction is used:
+	
+	I can be placed before V (5) and X (10) to make 4 and 9.
+	X can be placed before L (50) and C (100) to make 40 and 90.
 	C can be placed before D (500) and M (1000) to make 400 and 900.
-	Given an integer, convert it to a roman numeral.
-	 
+	Given an integer, convert it to a Roman numeral.
 	
 	Example 1:
-	Input: num = 3
+	Input: number = 3
 	Output: "III"
 	Explanation: 3 is represented as 3 ones.
 	
 	Example 2:
-	Input: num = 58
+	Input: number = 58
 	Output: "LVIII"
 	Explanation: L = 50, V = 5, III = 3.
 	
 	Example 3:
-	Input: num = 1994
+	Input: number = 1994
 	Output: "MCMXCIV"
 	Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
  * 
  * 
  * @author Er. Abraham Bisrat https://github.com/abrahammehari
- *
+ * 
  */
 public class IntegerToRoman {
 	static void p(Object line) { System.out.println(line); }
 	public static void main(String[] args) {
-		p(intToRoman(12345));
+//		p(intToRoman(12345));
+		p(intToRoman(58));
 	}
-	/* Approach
-		take the length of the number into consideration to build the respective roman equivalent
-		and parse out the to which group of the roman number group it belongs to.
-		and for parse out the details and add them the number of times of the 
+	/*  -> Approaches
+		take the length of the number into consideration to build the respective Roman equivalent
+		and parse out the to which group of the Roman number group it belongs to.
+		and for parse out the details and add them the number of times of the
 		digit(3 on the second digit is considered as 30 and will be given XXX accordingly)
-		
+		 
 		example -> 58 -> LV|||
 		
 		take the biggest possible and think about the rest after that.
@@ -63,10 +68,8 @@ public class IntegerToRoman {
 		This is 1000 + 900 + 90 + 4
 		
 		::
-		Also another approach would be to push each digit into a stack 
+		Also another approach would be to push each digit into a stack
 		and pop them out one by one and assign the Roman letters in a greedy approach.
-		
-		
 	 */
 	static String intToRoman(int num) {
 	    String inputNum = num + "";
@@ -78,13 +81,35 @@ public class IntegerToRoman {
 	    for(char digit : inputNum.toCharArray())
 	    	queue.add(Integer.parseInt(digit + ""));
 	    
-	    int index = 1;
+	    int index = 0;
 	    while(!queue.isEmpty()) {
 	    	int currentNum = (int) queue.poll();
-	    	p(currentNum);
+	    	while(currentNum > 0) {
+	    		if(currentNum > 4) {
+	    			romanNum += romanDigits[index];
+	    			currentNum -= 5;
+	    		} else {
+	    			romanNum += romanDigits[index];
+	    			currentNum--;
+	    		}
+	    	}
+	    	p(index);
+	    	index++;
 	    }
-	    
-	    return  " ";
+	    return  romanNum;
+	}
+	// Better working approach
+	static String intToRomanConv(int num) {
+		String input = num + "";
+		String romanNum = "";
+		char[] romanDigits = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+		
+		int index = 1000;
+		for(int i = input.length(); i >= 0; i--) {
+			int current = num % index;
+			while(current-- > 0) romanNum += romanDigits[6];
+		}
+		return romanNum;
 	}
 }
 
