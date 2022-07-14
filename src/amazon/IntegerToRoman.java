5b1593afe4d1ef5cb6dcf3b417bb14a90203ff4a
Abraham Bisrat <abrahammehari0@gmail.com>
2022-07-14 01:03:07 -0500

    clean up

--- src/amazon/IntegerToRoman.java
+++ src/amazon/IntegerToRoman.java
@@ -57,9 +57,6 @@ public class IntegerToRoman {
         int[] intCodes = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] romanCodes = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         
-         
-        p(intCodes.length + " " + romanCodes.length);
-        
         String result = "";
         for(int i = 0; i < intCodes.length; i++) {
         	while(num >= intCodes[i]) {
